acf456eab39493fb7f5c88acd75410f5d1962dad
Jared Buckner <jbuckner@wisc.edu>
2014-03-04 22:16:33 -0600

    Initialize Repository, Update GemFile; Configure database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MhtAppV2::Application.config.secret_key_base = '540965891cc297415d7b0acff4db8bc53f5f91c2bd88f552f355823199d78e4a5516e4bd7543c6051aae32cdda688f6dcd8e5ff0517e3b4db34f57d53a0669e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MhtAppV2::Application.config.session_store :cookie_store, key: '_mht_app_v2_session'
