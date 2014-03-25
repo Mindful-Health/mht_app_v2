074abeef4fb27f87006c952cad29b5c663372299
Chris Fernandez <chris2fernandez@gmail.com>
2014-03-24 22:06:35 -0500

    Use Heroku example file locations in clock.rb

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,9 +1,8 @@
 require 'clockwork'
 require 'clockwork/manager_with_database_tasks'
-require_relative './config/boot'
-require_relative './config/environment'
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 
-require 'clockwork'
 module Clockwork
   handler do |job|
     puts "Running #{job}"
