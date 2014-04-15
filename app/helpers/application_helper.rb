d4d68ffa99970ee2d88af7a7b3e7c557296cbd92
Olivia Rice <orice@wisc.edu>
2014-04-15 18:27:04 -0500

    form for cgs working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
 
-def current_user?(user)
-  user == current_user
-end
+	def current_user?(user)
+	  user == current_user
+	end
 
+	def full_title(page_title)
+		base_title = "Mindful Health"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+			
 end
