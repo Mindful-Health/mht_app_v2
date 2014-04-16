5af0a90c454b8d6f8570e60826fb56e4a5e0cf17
Jared Buckner <jbuckner@wisc.edu>
2014-04-16 17:02:45 -0500

    Add test data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-@prev
+@prev_spo2
+@prev_heart
 672.times do |i|
-@i = i
-if (@i == 0)
-	@spo2 = (89+Random.rand(99-89))
-end
 
-PatientData.create(device_id: 1, spo2: (89+Random.rand(99-89)), heart_rate: (50+Random.rand(150-50)), 
-					transmission_time: Time.at(13941612+(i*900)).to_datetime, patient_id: 1)
+	@i = i
+	if (@i == 0)
+		@prev_spo2 = 93
+		@prev_heart = 70
+
+		PatientData.create(device_id: 1, spo2: 93, heart_rate: 70, 
+						transmission_time: Time.at(13941612+(i*900)).to_datetime, patient_id: 1)
+	else
+		@factor_spo2 = [0.95, 1.05].sample
+		@factor_hr = [0.93, 1.07].sample
+		@prev_spo2 = @prev_spo2*@factor_spo2
+		@prev_heart = @prev_heart*@factor_hr
+
+
+		PatientData.create(device_id: 1, spo2: @prev_spo2, heart_rate: @prev_heart, 
+						transmission_time: Time.at(13941612+(i*900)).to_datetime, patient_id: 1)
+	end
+
+
 
 end
