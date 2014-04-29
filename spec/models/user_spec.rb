e71cc33e9d20a9805c0ea369aa95a4b4629ca6cc
Olivia Rice <orice@wisc.edu>
2014-04-29 17:28:39 -0500

    Final Project 2 assgn commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,35 @@ describe User do
   subject { @user }
 
   it { should respond_to(:name)  }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  
+  describe "when email is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
+    end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        #expect(@user).to be_valid
+      end
+    end
+  end
 
 end
