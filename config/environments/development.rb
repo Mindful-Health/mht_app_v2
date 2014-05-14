87b771a8e40349c32567ce9d0cd58a34b2fc9926
Olivia Rice <orice@wisc.edu>
2014-05-14 03:59:11 -0500

    h

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,14 +21,14 @@ MhtAppV2::Application.configure do
   #email delivery method
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = { 
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: 'login',
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  config.action_mailer.smtp_settings = {
+  address: “smtp.gmail.com”,
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: “plain”,
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
 
   # Print deprecation notices to the Rails logger.
