cd553d47ac1002c1e78271d260a72b64a2abaa17
Olivia Rice <orice@wisc.edu>
2014-05-14 03:30:26 -0500

    heroku trouble

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ MhtAppV2::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = { 
-    #:address => 'localhost', :port => 1025}
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: 'login',
