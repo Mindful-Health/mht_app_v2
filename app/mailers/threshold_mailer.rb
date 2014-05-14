3f5f2e70f28eac0a5ce35b060867e9f4838d82c1
Chris Fernandez <chris2fernandez@gmail.com>
2014-05-13 22:04:25 -0500

    Alter db:seed values for threshold testing

--- app/mailers/threshold_mailer.rb
+++ app/mailers/threshold_mailer.rb
@@ -1,10 +1,10 @@
 class ThresholdMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  #def notification_email(user)
-  #	@user = user
-  #	@url = 'http://example.com/login'
+  def notification_email(user)
+  	@user = user
+  	@url = 'http://example.com/login'
 
-  #	mail(to: @user.email, subject: 'Urgent Notification')
-  #end
+  	mail(to: @user.email, subject: 'Patient Vitals are Critical')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,16 @@
 		PatientData.create(device_id: 1, spo2: 93, heart_rate: 70, 
 						transmission_time: Time.at(13941612+(i*900)).to_datetime, patient_id: 1)
 	else
-		@factor_spo2 = [0.95, 1.05].sample
-		@factor_hr = [0.93, 1.07].sample
-		@prev_spo2 = @prev_spo2*@factor_spo2
-		@prev_heart = @prev_heart*@factor_hr
+		#@factor_spo2 = [0.95, 1.05].sample
+		#@factor_hr = [0.93, 1.07].sample
+		#@prev_spo2 = @prev_spo2*@factor_spo2
+		#@prev_heart = @prev_heart*@factor_hr
+		@prev_spo2 = 93
+		@prev_heart = 70
 
 
 		PatientData.create(device_id: 1, spo2: @prev_spo2, heart_rate: @prev_heart, 
 						transmission_time: Time.at(13941612+(i*900)).to_datetime, patient_id: 1)
 	end
-
-
 
 end
